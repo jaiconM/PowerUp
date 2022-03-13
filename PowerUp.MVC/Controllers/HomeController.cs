a4354066169b46638918f79d84331ab1b4f41b33
jaiconM <jaicon.marmitt@gmail.com>
2022-03-13 18:46:58 -0300

    adicionando o google maps com a carga dos marcadores

--- PowerUp.MVC/Controllers/HomeController.cs
+++ PowerUp.MVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PowerUp.App.Interfaces;
+using PowerUp.Domain.Core.Enums;
 using PowerUp.MVC.Models;
 using System.Diagnostics;
 
@@ -6,21 +8,32 @@ namespace PowerUp.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private const string IconeTipoMovel = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+        private const string IconeTipoVeicular = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+        private readonly IEstacaoRecargaApp _estacaoRecargaApp;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IEstacaoRecargaApp estacaoRecargaApp)
         {
-            _logger = logger;
+            _estacaoRecargaApp = estacaoRecargaApp;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            var estacoesDeRecarga = _estacaoRecargaApp.ListarTodos();
+            var model = new List<EstacoesDeCargaViewModel>();
+            foreach (var estacao in estacoesDeRecarga)
+            {
+                model.Add(
+                    new EstacoesDeCargaViewModel
+                    {
+                        Icone = estacao.Tipo == Tipo.Veicular ? IconeTipoVeicular : IconeTipoMovel,
+                        Titulo = $"{estacao.Nome} (Tipo: {estacao.Tipo})",
+                        Latitude = estacao.Latitude.ToString().Replace(',','.'),
+                        Longitude = estacao.Longitude.ToString().Replace(',', '.')
+                    }
+                );
+            }
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ PowerUp.MVC/Models/EstacoesDeCargaViewModel.cs
@@ -0,0 +1,10 @@
+namespace PowerUp.MVC.Models
+{
+    public class EstacoesDeCargaViewModel
+    {
+        public string Titulo { get; set; }
+        public string Icone { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
